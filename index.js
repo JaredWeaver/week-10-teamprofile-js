89da10fddc40d62451b30a84e0024df3fb1083d4
JaredWeav <jared.weaver2@gmail.com>
2021-02-04 13:34:23 -0500

    test html created, js started

--- index.js
+++ index.js
@@ -3,3 +3,40 @@ const jest = require('jest');
 const fs = require('fs');
 
 
+const initialQuestions = () => { inquirer
+
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the teams Manager\'\ s name?',
+            name:'managerName'
+        },
+        {
+            type: 'input',
+            message: 'What is the teams Manager\'\ s name?',
+            name:'managerName'
+        },
+        {
+            type: 'input',
+            message: 'What is the teams Manager\'\ s name?',
+            name:'managerName'
+        },
+        {
+            type: 'input',
+            message: 'What is the teams Manager\'\ s name?',
+            name:'managerName'
+        },
+{
+            type: 'input',
+            message: 'What is the teams Manager\'\ s name?',
+            name:'managerName'
+        },
+
+
+    ]);
+
+
+
+
+
+}
