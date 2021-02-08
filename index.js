0fa49a5c1063cc3d046c309310da5839941cc571
JaredWeav <jared.weaver2@gmail.com>
2021-02-08 15:05:08 -0500

    added in more inquirer prompts

--- index.js
+++ index.js
@@ -1,43 +1,162 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const manager = require('./lib/Manager');
-const engineer = require('./lib/Engineer');
-const intern = require('./lib/Intern');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 const teamMembersArr=[];
+const teamName= [];
 
-const prompt = () => { inquirer
+
+
+
+const startPrompt = () => { inquirer
+
+  .prompt([
+    {
+      type: 'input',
+      message: 'Welcome. To begin building your team, what is your team\'s name?',
+      name: 'teamname'
+    }
+  ])
+  .then((data) => {
+    console.log(data);
+    teamName.push(data.teamname);
+    addManager()
+  })
+}
+
+const addManager = () => { inquirer
 
     .prompt([
         {
             type: 'input',
-            message: 'What is the Manager\'\ s name?',
+            message: 'What is the manager\'s name?',
             name:'name'
         },
         {
             type: 'input',
-            message: 'What is the Manager\'s employee ID?',
+            message: 'What is the manager\'s employee ID?',
             name:'id'
         },
         {
             type: 'input',
-            message: 'What is the Manager\'s email address?',
+            message: 'What is the manager\'s email address?',
             name:'email'
         },
         {
             type: 'input',
-            message: 'What is the Manager\'s office number?',
+            message: 'What is the manager\'s office number?',
             name:'officeNumber'
         },
 
-    ]).then((response, ) => {
-        console.log(response);
+    ]).then(function({name, id, email, officeNumber}) {
+       let newManager;
+       newManager = new Manager(name, id, email, officeNumber);
+       teamMembersArr.push(newManager);
+       addEmployee();
+
 
     });
 
+}
 
+const addEmployee = () => { inquirer
 
+  .prompt([
+  {
+    type: 'list',
+    message: 'Select from the options below to add members to your team. Want to add more members?',
+    name:'addOrEnd',
+    choices: ['Yes, I would like to add an engineer', 
+              'Yes, I would like to add an intern', 
+              'Nope, I am finished.']
+  }
 
+  ]).then(function(choices){
+    console.log(choices.addOrEnd)
+    const addOrEnd = choices.addOrEnd;
+    if(addOrEnd.includes('intern')){
+      addIntern()
+    }
+    if(addOrEnd.includes('engineer')){
+      addEngineer()
+    }
+    if(addOrEnd.includes('finished')){
+      console.log(teamMembersArr);
+    };
+  });
+}
+
+const addEngineer = () => { inquirer
+
+  .prompt([
+      {
+          type: 'input',
+          message: 'What is the engineer\'s name?',
+          name:'name'
+      },
+      {
+          type: 'input',
+          message: 'What is the engineer\'s employee ID?',
+          name:'id'
+      },
+      {
+          type: 'input',
+          message: 'What is the engineer\'s email address?',
+          name:'email'
+      },
+      {
+          type: 'input',
+          message: 'What is the engineers\'s Github username?',
+          name:'github'
+      },
+
+  ]).then(function({name, id, email, github}) {
+     console.log({name, id, email, github});
+     let newEngineer;
+       newEngineer = new Engineer(name, id, email, github);
+       teamMembersArr.push(newEngineer);
+     
+     addEmployee();
+  });
+
+}
+
+const addIntern = () => { inquirer
+
+  .prompt([
+      {
+          type: 'input',
+          message: 'What is the intern\'s name?',
+          name:'name'
+      },
+      {
+          type: 'input',
+          message: 'What is the intern\'s employee ID?',
+          name:'id'
+      },
+      {
+          type: 'input',
+          message: 'What is the intern\'s email address?',
+          name:'email'
+      },
+      {
+          type: 'input',
+          message: 'What school does the intern attend?',
+          name:'school'
+      },
+
+  ]).then(function({name, id, email, school}) {
+     console.log({name, id, email, school});
+     let newIntern;
+       newIntern = new Intern(name, id, email, school);
+       teamMembersArr.push(newIntern);
+     
+     addEmployee();
+
+  });
 
 }
 
@@ -90,25 +209,25 @@ function renderHTML(role){
 
 }
 
-function addEmployeeCard(role, object) {
-    if(role === 'Manager'){
-        return `
-    <div class="col-md-3 mx-auto card employeeCard border bg-custom">
-        <div class="card-header">
-            <h3>Jared</h3>
-            <h5>Manager  <i class="fas fa-clipboard"></i></h5>
-        </div>
-        <p class="card-text">Employee ID: 1</p>
-        <p class="card-text"><a href="mailto:">jared.weaver2@gmail.com</a></p>
-        <p class="card-text">Office Number: 4</p>
-    </div>
-    `
-    }
+function addEmployeeCard(role) {
+    // if(role === 'Manager'){
+    //     return `
+    // <div class="col-md-3 mx-auto card employeeCard border bg-custom">
+    //     <div class="card-header">
+    //         <h3>Jared</h3>
+    //         <h5>Manager  <i class="fas fa-clipboard"></i></h5>
+    //     </div>
+    //     <p class="card-text">Employee ID: 1</p>
+    //     <p class="card-text"><a href="mailto:">jared.weaver2@gmail.com</a></p>
+    //     <p class="card-text">Office Number: 4</p>
+    // </div>
+    // `
+    // }
 
 
 }
 
 function init(){
-    prompt();
+    startPrompt();
 }
 init()
